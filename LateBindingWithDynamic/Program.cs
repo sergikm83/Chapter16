92e067aac5d17f8f6a3cc0d952b9990af249751c
Sergey Misko <spy_3462@mail.ru>
2022-07-22 16:44:17 +0400

    добавил метод AddWithDynamic() к классу Program для вызова метода Add() класса SimpleMath из библиотеки классов MathLibrary при помощи dynamic

--- LateBindingWithDynamic/Program.cs
+++ LateBindingWithDynamic/Program.cs
@@ -23,12 +23,28 @@ namespace LateBindingWithDynamic
                 MethodInfo mi = math.GetMethod("Add");
                 // Вызвать метод Add с параметрами.
                 object[] args = { 10, 70 };
-                Console.WriteLine("Results is: {0}", mi.Invoke(obj,args);
+                Console.WriteLine("Results is: {0}", mi.Invoke(obj, args);
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
         }
+        private static void AddWithDynamic()
+        {
+            Assembly asm = Assembly.Load("MathLibrary");
+            try
+            {
+                // Получить метаданные для типа SimpleMath.
+                Type math = asm.GetType("MathLibrary.SimpleMath");
+                // Создать объект SimpleMath на лету.
+                dynamic obj = Activator.CreateInstance(math);
+                // Обратите внимание, насколько легко теперь вызывать метод Add().
+                Console.WriteLine("Results is: {0}", obj.Add(10, 70));
+            }
+            catch(Microsoft.CSharp.RuntimeBinder.RuntimeBinderException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
     }
-}
+    }

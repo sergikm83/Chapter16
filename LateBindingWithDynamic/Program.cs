d5497865921b0eb9b644ccbf413d7130737dbc98
Sergey Misko <spy_3462@mail.ru>
2022-07-22 16:40:12 +0400

    добавил метод AddWithReflection() в класс Program для вызова метода Add() класса SimpleMath из библиотеки MathLibrary

--- LateBindingWithDynamic/Program.cs
+++ LateBindingWithDynamic/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 
 namespace LateBindingWithDynamic
 {
@@ -7,6 +8,27 @@ namespace LateBindingWithDynamic
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        private static void AddWithReflection()
+        {
+            Assembly asm = Assembly.Load("MathLibrary");
+            try
+            {
+                // Получить метаданные для типа SimpleMath.
+                Type math = asm.GetType("MathLibrary.SimpleMath");
+                // Создать объект SimpleMAth на лету.
+                object obj = Activator.CreateInstance(math);
+                // Получить информацию о методе Add().
+                MethodInfo mi = math.GetMethod("Add");
+                // Вызвать метод Add с параметрами.
+                object[] args = { 10, 70 };
+                Console.WriteLine("Results is: {0}", mi.Invoke(obj,args);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }

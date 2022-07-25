cb8e1aa3e36dbd8e0e818a702a67284767e38e59
Sergey Misko <spy_3462@mail.ru>
2022-07-25 16:16:06 +0400

    добавил метод ExportToExcel() экспорта переданных данных List<Car> в файл Excel

--- ExportDataToOfficeApp/Program.cs
+++ ExportDataToOfficeApp/Program.cs
@@ -17,5 +17,31 @@ namespace ExportDataToOfficeApp
                 new Car{ Color = "Yellow", Make = "BMW", PetName = "Davie" }
             };
         }
+        static void ExportToExcel(List<Car> carInStock)
+        {
+            // Загрузить Excel и затем создать новую пустую рабочую книгу.
+            Excel.Application excelApp = new Excel.Application();
+            excelApp.Workbooks.Add();
+            Excel._Worksheet workSheet = excelApp.ActiveSheet;
+            // Установить заголовки столбцов в ячейках.
+            workSheet.Cells[1, "A"] = "Make";
+            workSheet.Cells[1, "B"] = "Color";
+            workSheet.Cells[1, "C"] = "Pet Name";
+            // Отобразить все данные из List<Car> на ячейки электронной таблицы.
+            int row = 1;
+            foreach(Car c in carInStock)
+            {
+                row++;
+                workSheet.Cells[row, "A"] = c.Make;
+                workSheet.Cells[row, "B"] = c.Color;
+                workSheet.Cells[row, "C"] = c.PetName;
+            }
+            // Придать смпатичный вид табличным данным.
+            workSheet.Range["A1"].AutoFormat(Excel.XlRangeAutoFormat.xlRangeAutoFormatClassic2);
+            // Сохранить файл, завершить работу Excel и отобразить сообщение пользователю.
+            workSheet.SaveAs2($@"{Environment.CurrentDirectory}\Inventory.xlsx");
+            excelApp.Quit();
+            Console.WriteLine("The inventory.xlsx file has been saved to your app filder");
+        }
     }
 }

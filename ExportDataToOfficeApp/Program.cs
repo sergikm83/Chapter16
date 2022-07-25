3545139e3441bd8d0b2a28da6004a20a54d516f8
Sergey Misko <spy_3462@mail.ru>
2022-07-25 15:22:34 +0400

    добавил новый проект ExportDataToOfficeApp

---
+++ ExportDataToOfficeApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExportDataToOfficeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
